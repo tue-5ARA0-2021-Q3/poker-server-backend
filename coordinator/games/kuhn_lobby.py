d5c0e0309e8a2aa85deafed5dd7de516dfe5ce15
Dmitri Bagaev <bvdmitri@gmail.com>
2020-11-12 15:18:23 +0100

    feat(): Refactoring, improvements

---
+++ coordinator/games/kuhn_lobby.py
@@ -0,0 +1,202 @@
+import threading
+import queue
+import random
+
+from typing import List
+
+from coordinator.games.kuhn_game import KuhnRootChanceGameState
+from coordinator.games.kuhn_constants import CARDS_DEALINGS, NEXT
+from coordinator.models import Game
+
+
+class KuhnGameLobbyStageMessage(object):
+
+    def __init__(self, state, actions):
+        self.state = state
+        self.actions = actions
+
+
+class KuhnGameLobbyStageError(object):
+
+    def __init__(self, error):
+        self.error = error
+
+
+class KuhnGameLobbyPlayer(object):
+
+    def __init__(self, player_id: int, bank: int):
+        self.player_id = player_id
+        self.bank = bank
+        self.channel = queue.Queue()
+
+    def send_message(self, message: KuhnGameLobbyStageMessage):
+        self.channel.put(message)
+
+    def send_error(self, error: KuhnGameLobbyStageError):
+        self.channel.put(error)
+
+
+class KuhnGameLobbyStage(object):
+
+    def __init__(self, lobby):
+        self._root = KuhnRootChanceGameState(CARDS_DEALINGS)
+        self._stage = self._root.play(random.choice(CARDS_DEALINGS))
+        self._cards = self._stage.cards
+
+    def cards(self):
+        return self._cards
+
+    def card(self, index):
+        return self._cards[index]
+
+    def actions(self):
+        return self._stage.actions
+
+    def play(self, action):
+        self._stage = self._stage.play(action)
+
+    def is_terminal(self):
+        return self._stage.is_terminal()
+
+    def inf_set(self):
+        return self._stage.inf_set()
+
+    def secret_inf_set(self):
+        return self._stage.secret_inf_set()
+
+
+class KuhnGameLobby(object):
+    InitialBank = 5
+    MessagesTimeout = 5
+
+    def __init__(self, game_id: str):
+        self.lock = threading.Lock()
+        self.game_id = game_id
+        self.stages = []
+        self.channel = queue.Queue()
+
+        # private fields
+        self._closed = threading.Event()
+        self._lobby_coordinator_thread = None
+        self._player_connection_barrier = threading.Barrier(3)
+
+        self._players = {}
+        self._player_opponent = {}
+
+    def close(self):
+        with self.lock:
+            self._closed.set()
+
+    def is_closed(self) -> bool:
+        with self.lock:
+            return self._closed.is_set()
+
+    def get_players(self) -> List[KuhnGameLobbyPlayer]:
+        return list(self._players.values())
+
+    def get_player(self, player_id: str) -> KuhnGameLobbyPlayer:
+        return self._players[player_id]
+
+    def get_player_ids(self) -> List[str]:
+        return list(self._players.keys())
+
+    def get_num_players(self) -> int:
+        return len(self.get_player_ids())
+
+    def get_player_opponent(self, player_id: str) -> str:
+        return self._player_opponent[player_id]
+
+    def get_player_channel(self, player_id: str) -> queue.Queue:
+        return self._players[player_id].channel
+
+    def get_random_player_id(self) -> str:
+        return random.choice(list(self._players.keys()))
+
+    def start(self):
+        # First player which hits this function starts a separate thread with a game coordinator
+        # Ref: play_lobby(lobby)
+        with self.lock:
+            if self._lobby_coordinator_thread is None:
+                self._lobby_coordinator_thread = threading.Thread(target = game_lobby_coordinator,
+                                                                  args = (self, KuhnGameLobby.MessagesTimeout))
+                self._lobby_coordinator_thread.start()
+
+    def register_player(self, player_id: int):
+        with self.lock:
+            # Check if lobby is already full or throw an exception otherwise
+            if self.get_num_players() >= 2:
+                raise Exception('Game lobby is full')
+
+            # For each player we create a separate channel for messages between game coordinator and player
+            self._players[player_id] = KuhnGameLobbyPlayer(player_id, bank = KuhnGameLobby.InitialBank)
+
+            # If both players are connected we set corresponding ids to self._player_opponent dictionary for easy lookup
+            if self.get_num_players() == 2:
+                player_ids = self.get_player_ids()
+                player1_id, player2_id = player_ids[0], player_ids[1]
+
+                self._player_opponent[player1_id] = player2_id
+                self._player_opponent[player2_id] = player1_id
+
+                # Update database entry of the game with corresponding player ids and mark it as started
+                game_db = Game.objects.get(id = self.game_id)
+                game_db.player_1 = player1_id
+                game_db.player_2 = player2_id
+                game_db.is_started = True
+                game_db.save(update_fields = ['player_1', 'player_2', 'is_started'])
+
+    def wait_for_players(self):
+        try:
+            self._player_connection_barrier.wait(timeout = 120)
+        except threading.BrokenBarrierError:
+            raise Exception('Timeout waiting for another player to connect')
+
+
+def game_lobby_coordinator(lobby: KuhnGameLobby, messages_timeout: int):
+    lobby.wait_for_players()
+
+    try:
+        stage = KuhnGameLobbyStage(lobby)
+
+        players = lobby.get_players()
+        player_id_turn = lobby.get_random_player_id()
+
+        for player in players:
+            if player.player_id == player_id_turn:
+                player.send_message(KuhnGameLobbyStageMessage(f'{stage.card(0)}', stage.actions()))
+            else:
+                player.send_message(KuhnGameLobbyStageMessage(f'{stage.card(1)}', ['WAIT']))
+
+        while not lobby.is_closed() or not lobby.channel.empty():
+            try:
+                message = lobby.channel.get(timeout = messages_timeout)
+
+                print(f'Received a message: {message}')
+
+                if message['player_id'] == player_id_turn:
+                    stage.play(message['action'])
+
+                    if stage.is_terminal():
+                        for player in players:
+                            player.send_message(KuhnGameLobbyStageMessage(f'END:{stage.inf_set()}', ['END']))
+                        lobby.close()
+                    else:
+                        player_id_turn = lobby.get_player_opponent(player_id_turn)
+                        lobby.get_player(player_id_turn).send_message(KuhnGameLobbyStageMessage(stage.secret_inf_set(), stage.actions()))
+                elif message['player_id'] == lobby.get_player_opponent(player_id_turn) and \
+                        (message['action'] == 'START' or message['action'] == 'WAIT'):
+                    continue
+                else:
+                    print(f'Warn: unexpected message: {message}')
+                    continue
+
+            except queue.Empty:
+                raise Exception(f'There was no message from a player for more than {messages_timeout} sec.')
+
+    except Exception as e:
+        for player in lobby.get_players():
+            # noinspection PyBroadException
+            try:
+                player.send_error(KuhnGameLobbyStageError(e))
+            except Exception:
+                pass
--- coordinator/services.py
+++ coordinator/services.py
@@ -1,158 +1,11 @@
 import grpc
-import time
 import sys
 import threading
-import queue
-import random
 
-from coordinator.games.kuhn_game import KuhnRootChanceGameState
-from coordinator.games.kuhn_constants import CARDS_DEALINGS, NEXT
+from coordinator.games.kuhn_lobby import KuhnGameLobby
 from django_grpc_framework.services import Service
 from coordinator.models import Game, Player, GameTypes
 from proto.game import game_pb2
-
-
-def play_lobby(lobby, messages_timeout):
-    lobby.wait_for_players()
-
-    try:
-        root = KuhnRootChanceGameState(CARDS_DEALINGS)
-        stage = root.play(random.choice(CARDS_DEALINGS))
-        cards = stage.cards
-
-        players = lobby.get_players()
-        player_id_turn = lobby.get_random_player_id()
-
-        for player_id, player_data in players:
-            if player_id == player_id_turn:
-                lobby.send_message_to_player(player_id, {'state': f'{cards[0]}', 'available_actions': stage.actions})
-            else:
-                lobby.send_message_to_player(player_id, {'state': f'{cards[1]}', 'available_actions': ['WAIT']})
-
-        while not lobby.is_closed() or not lobby.get_main_channel().empty():
-            try:
-                message = lobby.get_main_channel().get(timeout = messages_timeout)
-
-                print(f'Received a message: {message}')
-
-                if message['player_id'] == player_id_turn:
-                    stage = stage.play(message['action'])
-
-                    if stage.is_terminal():
-                        for player_id, player_data in players:
-                            try:
-                                lobby.send_message_to_player(player_id, {
-                                    'state': f'END:{stage.inf_set()}', 'available_actions': ['END']
-                                })
-                            except Exception:
-                                pass
-                        lobby.close()
-                    else:
-                        player_id_turn = lobby.get_player_opponent(player_id_turn)
-                        lobby.send_message_to_player(player_id_turn, {
-                            'state': stage.secret_inf_set(), 'available_actions': stage.actions
-                        })
-                elif message['player_id'] == lobby.get_player_opponent(player_id_turn) and \
-                        (message['action'] == 'START' or message['action'] == 'WAIT'):
-                    continue
-                else:
-                    print(f'Warn: unexpected message: {message}')
-                    continue
-
-            except queue.Empty:
-                raise Exception(f'There was no message from a player for more than {messages_timeout} sec.')
-
-    except Exception as e:
-        for player_id, player_data in lobby.get_players():
-            try:
-                player_data['channel'].put({'error': e})
-            except Exception:
-                pass
-
-
-class GameLobby(object):
-    InitialBank = 5
-    MessagesTimeout = 5
-
-    def __init__(self, game_id):
-        self.lock = threading.Lock()
-        self.game_id = game_id
-        self.stage = None
-        self.lobby_thread = None
-
-        # private fields
-        self._closed = threading.Event()
-        self._main_channel = queue.Queue()
-        self._players = {}
-        self._player_opponent = {}
-        self._player_connection_barrier = threading.Barrier(3)
-
-    def close(self):
-        with self.lock:
-            self._closed.set()
-
-    def is_closed(self):
-        with self.lock:
-            return self._closed.is_set()
-
-    def get_main_channel(self):
-        return self._main_channel
-
-    def get_players(self):
-        return self._players.items()
-
-    def get_player_opponent(self, player_id):
-        return self._player_opponent[player_id]
-
-    def get_player_channel(self, player_id):
-        return self._players[player_id]['channel']
-
-    def send_message_to_player(self, player_id, message):
-        self.get_player_channel(player_id).put(message)
-
-    def start(self):
-        # First player which hits this function starts a separate thread with a game coordinator
-        # Ref: play_lobby(lobby)
-        with self.lock:
-            if self.lobby_thread is None:
-                self.lobby_thread = threading.Thread(target = play_lobby, args = (self, GameLobby.MessagesTimeout))
-                self.lobby_thread.start()
-
-    def register_player(self, player_id):
-        with self.lock:
-            # Check if lobby is already full or throw an exception otherwise
-            if len(list(self._players.keys())) >= 2:
-                raise Exception('Game lobby is full')
-
-            # For each player we create a separate channel for messages between game coordinator and player
-            self._players[player_id] = {
-                'channel': queue.Queue(),
-                'bank': GameLobby.InitialBank
-            }
-
-            # If both players are connected we set corresponding ids to self._player_opponent dictionary for easy lookup
-            if len(self._players.keys()) == 2:
-                player_ids = list(self._players.keys())
-                player1_id, player2_id = player_ids[0], player_ids[1]
-
-                self._player_opponent[player1_id] = player2_id
-                self._player_opponent[player2_id] = player1_id
-
-                # Update database entry of the game with corresponding player ids and mark it as started
-                game_db = Game.objects.get(id = self.game_id)
-                game_db.player_1 = player1_id
-                game_db.player_2 = player2_id
-                game_db.is_started = True
-                game_db.save(update_fields = ['player_1', 'player_2', 'is_started'])
-
-    def get_random_player_id(self):
-        return random.choice(list(self._players.keys()))
-
-    def wait_for_players(self):
-        try:
-            self._player_connection_barrier.wait(timeout = 120)
-        except threading.BrokenBarrierError:
-            raise Exception('Timeout waiting for another player to connect')
 
 
 class GameCoordinatorService(Service):
@@ -209,16 +62,15 @@ class GameCoordinatorService(Service):
         lobby.register_player(token)
         lobby.wait_for_players()
 
-        lobby_channel = lobby.get_main_channel()
         player_channel = lobby.get_player_channel(token)
 
         try:
             for message in request:
                 if message.action != 'CONNECT' and message.action != 'WAIT':
-                    lobby_channel.put({'player_id': token, 'action': message.action})
+                    lobby.channel.put({'player_id': token, 'action': message.action})
                 response = player_channel.get()
-                state = response['state']
-                actions = response['available_actions']
+                state = response.state
+                actions = response.actions
                 yield game_pb2.PlayGameResponse(state = state, available_actions = actions)
         except grpc.RpcError:
             pass
@@ -226,6 +78,7 @@ class GameCoordinatorService(Service):
             print('Unhandled exception: ', e)
             print(sys.exc_info())
         finally:
+            # lobby_channel.put({'player_id': token, 'action': 'DISCONNECTED'})
             GameCoordinatorService.remove_game_lobby_instance(game_id)
 
         # # time.sleep(1.0)
@@ -249,19 +102,19 @@ class GameCoordinatorService(Service):
         #             return
 
     @staticmethod
-    def create_game_lobby_instance(game_id):
+    def create_game_lobby_instance(game_id: str) -> KuhnGameLobby:
         lobby = None
         with GameCoordinatorService.games_lock:
             game_lobbies = list(filter(lambda game: game.game_id == game_id, GameCoordinatorService.game_lobbies))
             if len(game_lobbies) == 0:
-                lobby = GameLobby(game_id)
+                lobby = KuhnGameLobby(game_id)
                 GameCoordinatorService.game_lobbies.append(lobby)
             else:
                 lobby = game_lobbies[0]
         return lobby
 
     @staticmethod
-    def get_game_lobby_instance(game_id):
+    def get_game_lobby_instance(game_id: str) -> KuhnGameLobby:
         lobby = None
         with GameCoordinatorService.games_lock:
             game_lobbies = list(filter(lambda game: game.game_id == game_id, GameCoordinatorService.game_lobbies))
@@ -272,7 +125,7 @@ class GameCoordinatorService(Service):
         return lobby
 
     @staticmethod
-    def remove_game_lobby_instance(game_id):
+    def remove_game_lobby_instance(game_id: str):
         with GameCoordinatorService.games_lock:
             game_lobbies = list(filter(lambda game: game.game_id == game_id, GameCoordinatorService.game_lobbies))
             for lobby in game_lobbies:
